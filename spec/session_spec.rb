3bf1aa5d776d87f5a9855962bb45c133b4ed1b55
Tomás Pollak <tomaspollak@gmail.com>
2019-04-30 15:56:42 -0400

    Specs

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -15,4 +15,54 @@ RSpec.describe Dafiti::Session do
       expect(session.run(:feed_list, FeedListId: 123)).to eq response
     end
   end
+
+  describe "#run_and_wait" do
+    it "explodes if request doesnt have a request_id" do
+      session = described_class.new(client)
+      response = double('Response')
+
+      expect(client).to receive(:request) do |action|
+        expect(action).to be_a Dafiti::Actions::FeedList
+      end.and_return response
+
+      expect do
+        session.run_and_wait(:feed_list, FeedListId: 123)
+      end.to raise_error RuntimeError
+    end
+
+    it "runs action" do
+      session = described_class.new(client)
+
+      response_one = double('Response', request_id: 1234)
+      expect(client).to receive(:request) do |action|
+        expect(action).to be_a Dafiti::Actions::FeedList
+      end.once.and_return response_one
+
+      response_two = double('Response', status: 'OK') # not queued/processing
+      expect(client).to receive(:request) do |action|
+        expect(action).to be_a Dafiti::Actions::FeedStatus
+      end.once.and_return response_two
+
+      expect(session.run_and_wait(:feed_list, FeedListId: 123)).to eq response_two
+    end
+
+    it "yields block if passed" do
+      session = described_class.new(client)
+
+      response_one = double('Response', request_id: 1234)
+      expect(client).to receive(:request) do |action|
+        expect(action).to be_a Dafiti::Actions::FeedList
+      end.once.and_return response_one
+
+      response_two = double('Response', status: 'OK') # not queued/processing
+      expect(client).to receive(:request) do |action|
+        expect(action).to be_a Dafiti::Actions::FeedStatus
+      end.once.and_return response_two
+
+      session.run_and_wait(:feed_list, FeedListId: 123) do |resp|
+        expect(resp).to eq response_one
+      end
+    end
+  end
+
 end
